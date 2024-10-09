56d6840caa6850e016bb800efa23a57c069f1412
rogereduaardo <129445500+rogereduaardo@users.noreply.github.com>
2024-10-09 22:43:46 +0000

    Directories

--- main.py
+++ main.py
@@ -276,4 +276,88 @@ print("Element 2,0,1 of Tuple: ", NestedTuple[2][0][1])
 
 print("disco".find('s'))
 
+# Create the dictionary
+Dict = {
+"key1": 1, "key2": "2", "key3": [3, 3, 3], 
+"key4": (4, 4, 4), ('key5'): 5, (0, 1): 6}
 
+# Access to the value by the key
+print(Dict["key1"])
+print(Dict[(0, 1)])
+
+# Create a sample dictionary
+release_year_dict = {"Thriller": "1982", "Back in Black": "1980", \
+"The Dark Side of the Moon": "1973", "The Bodyguard": "1992", \
+"Bat Out of Hell": "1977", "Their Greatest Hits (1971-1975)": "1976", \
+"Saturday Night Fever": "1977", "Rumours": "1977"}
+
+# Get value by keys
+print(release_year_dict['Thriller']) 
+print(release_year_dict['The Bodyguard'])
+print("\n")
+
+# Get all the keys in dictionary
+print(release_year_dict.keys()) 
+print("\n")
+
+# Get all the values in dictionary
+print(release_year_dict.values())
+print("\n")
+
+# Append value with key into dictionary
+release_year_dict['Graduation'] = '2007'
+
+print(release_year_dict)
+print("\n")
+
+# Delete entries by key
+del(release_year_dict['Thriller'])
+del(release_year_dict['Graduation'])
+
+print(release_year_dict)
+print("\n")
+
+# Verify the key is in the dictionary
+print('The Bodyguard' in release_year_dict)
+print("\n")
+
+# Example
+
+# Create an emtpy dictionary
+inventory = {}
+
+# Store the first product details in variable
+ProductNo1 = "Mobile Phone"
+ProductNo1_quantity = 5
+ProductNo1_price = 20000
+ProductNo1_releaseYear= 2020
+
+# Add the details in inventory
+inventory["ProductNo1"] = ProductNo1
+inventory["ProductNo1_quantity"] = ProductNo1_quantity
+inventory["ProductNo1_price"] = ProductNo1_price
+inventory["ProductNo1_releaseYear"] =ProductNo1_releaseYear
+
+# Store the second product details in variable
+ProductNo2 = "Laptop"
+ProductNo2_quantity = 10
+ProductNo2_price = 50000
+ProductNo2_releaseYear= 2023
+
+# Add the details in inventory
+inventory["ProductNo2"]= ProductNo2
+inventory["ProductNo2_quantity"]= ProductNo2_quantity
+inventory["ProductNo2_price"]= ProductNo2_price
+inventory["ProductNo2_releaseYear"]= ProductNo2_releaseYear
+
+# Display the products in the inventory
+print(inventory)
+print("\n")
+
+# Chek if ProductNo1_releaseYear and ProductNo2_releaseYear is in the inventory
+print("ProductNo1_releaseYear" in inventory)
+print("ProductNo2_releaseYear" in inventory)
+
+# Delete release year of both products from the invenotry
+del(inventory['ProductNo1_releaseYear'])
+del(inventory['ProductNo2_releaseYear'])   
